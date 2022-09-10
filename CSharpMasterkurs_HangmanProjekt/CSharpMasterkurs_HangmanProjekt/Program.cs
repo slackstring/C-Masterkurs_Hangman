28395cde7d1b8658032d51540c1a231b41af60af
slackstring <slackstring88@gmail.com>
2022-09-10 20:23:38 +0200

    Vorbereitung

---
+++ CSharpMasterkurs_HangmanProjekt/CSharpMasterkurs_HangmanProjekt/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ProjektHangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            static void MainMenu()
+            {
+
+            }
+
+            static void StartGame()
+            {
+
+            }
+
+            static void GameLoop()
+            {
+
+            }
+        }
+    }
+}
